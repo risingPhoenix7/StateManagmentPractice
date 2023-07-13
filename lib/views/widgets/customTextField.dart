65accac1c60e3bc6697300fd5fbab635d4b6cc35
risingPhoenix7 <achinthyahebbar@gmail.com>
2023-07-13 20:18:20 +0530

    added smiley button in keyboard with proper focus handling

--- lib/views/widgets/customTextField.dart
+++ lib/views/widgets/customTextField.dart
@@ -1,7 +1,8 @@
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jio_task/viewmodel/chatBloc.dart';
-
+import 'package:flutter/foundation.dart' as foundation;
 import '../../models/message/message.dart';
 import '../../viewmodel/chatEvent.dart';
 import '../../viewmodel/chatState.dart';
@@ -16,7 +17,18 @@ class CustomTextField extends StatefulWidget {
 
 class _CustomTextFieldState extends State<CustomTextField> {
   bool _isComposing = false;
+  bool emojiShowing = false;
+  final key = GlobalKey<EmojiPickerState>();
+  FocusNode focusNode = FocusNode();
+
   final TextEditingController _textController = TextEditingController();
+
+  _onBackspacePressed() {
+    _textController
+      ..text = _textController.text.characters.toString()
+      ..selection = TextSelection.fromPosition(
+          TextPosition(offset: _textController.text.length));
+  }
 
   void _handleSubmitted(BuildContext context) {
     final bloc = BlocProvider.of<ChatBloc>(context);
@@ -33,8 +45,7 @@ class _CustomTextFieldState extends State<CustomTextField> {
     setState(() {
       _isComposing = false;
     });
-    FocusScope.of(context).unfocus();
-
+    focusNode.unfocus();
   }
 
   @override
@@ -43,9 +54,9 @@ class _CustomTextFieldState extends State<CustomTextField> {
       data: IconThemeData(color: Theme.of(context).primaryColor),
       child: Column(
         children: [
-          BlocBuilder<ChatBloc, ChatState>( builder: (context, state) {
+          BlocBuilder<ChatBloc, ChatState>(builder: (context, state) {
             if (state is ReplyModeEnabled && state.isLeft == widget.isLeft) {
-              return  Container(
+              return Container(
                 padding: const EdgeInsets.symmetric(
                   vertical: 8.0,
                   horizontal: 20.0,
@@ -83,8 +94,25 @@ class _CustomTextFieldState extends State<CustomTextField> {
             margin: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Row(
               children: <Widget>[
+                Container(
+                  child: IconButton(
+                      icon: const Icon(Icons.emoji_emotions),
+                      onPressed: () {
+                        setState(() {
+                          if (emojiShowing) {
+                            emojiShowing = false;
+                            FocusScope.of(context).requestFocus(focusNode);
+                          } else {
+                            emojiShowing = true;
+                            focusNode.unfocus();
+                            focusNode.canRequestFocus = true;
+                          }
+                        });
+                      }),
+                ),
                 Flexible(
                   child: TextField(
+                    focusNode: focusNode,
                     controller: _textController,
                     maxLines: null,
                     onChanged: (String text) {
@@ -92,7 +120,15 @@ class _CustomTextFieldState extends State<CustomTextField> {
                         _isComposing = text.isNotEmpty;
                       });
                     },
+                    onTap: () {
+                      setState(() {
+                        emojiShowing = false;
+                        FocusScope.of(context).requestFocus(focusNode);
+                      });
+                    },
                     decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      // Remove default bottom underline
                       hintText: 'Send a message',
                       contentPadding: EdgeInsets.all(20.0),
                     ),
@@ -109,8 +145,56 @@ class _CustomTextFieldState extends State<CustomTextField> {
               ],
             ),
           ),
+          Offstage(
+            offstage: !emojiShowing,
+            child: SizedBox(
+                height: 250,
+                child: EmojiPicker(
+                  key: key,
+                  textEditingController: _textController,
+                  onBackspacePressed: _onBackspacePressed,
+                  config: Config(
+                    columns: 7,
+                    emojiSizeMax: 32 *
+                        (foundation.defaultTargetPlatform == TargetPlatform.iOS
+                            ? 1.30
+                            : 1.0),
+                    verticalSpacing: 0,
+                    horizontalSpacing: 0,
+                    gridPadding: EdgeInsets.zero,
+                    initCategory: Category.RECENT,
+                    bgColor: const Color(0xFFF2F2F2),
+                    indicatorColor: Colors.blue,
+                    iconColor: Colors.grey,
+                    iconColorSelected: Colors.blue,
+                    backspaceColor: Colors.blue,
+                    skinToneDialogBgColor: Colors.white,
+                    skinToneIndicatorColor: Colors.grey,
+                    enableSkinTones: true,
+                    recentTabBehavior: RecentTabBehavior.RECENT,
+                    recentsLimit: 28,
+                    replaceEmojiOnLimitExceed: false,
+                    noRecents: const Text(
+                      'No Recents',
+                      style: TextStyle(fontSize: 20, color: Colors.black26),
+                      textAlign: TextAlign.center,
+                    ),
+                    loadingIndicator: const SizedBox.shrink(),
+                    tabIndicatorAnimDuration: kTabScrollDuration,
+                    categoryIcons: const CategoryIcons(),
+                    buttonMode: ButtonMode.MATERIAL,
+                    checkPlatformCompatibility: true,
+                  ),
+                )),
+          ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
   }
 }
